60a3193c595ec0cd54281920663bae68e576c589
Mikko Mattila <9086012+mikkomattila@users.noreply.github.com>
2025-10-26 12:10:52 +0200

    Extract progressor find logic from main.

--- src/main.rs
+++ src/main.rs
@@ -3,67 +3,38 @@
 #![deny(clippy::panic)]
 #![deny(unused_must_use)]
 
-use btleplug::api::{Central, Manager as _, Peripheral as _, ScanFilter};
+use btleplug::api::{Central, Manager as _, Peripheral};
 use btleplug::platform::Manager;
 use std::error::Error;
-use std::time::Duration;
-use tokio::time;
 
 mod progressor;
-
-const SCAN_DURATION: Duration = Duration::from_secs(10);
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let manager = Manager::new().await?;
     let adapters = manager.adapters().await?;
-    if adapters.is_empty() {
-        eprintln!("No Bluetooth adapters found, exiting...");
-        return Ok(());
-    }
-
-    for adapter in adapters.into_iter() {
-        println!(
-            "Scanning for devices using {}.",
-            adapter.adapter_info().await?
-        );
-
-        adapter.start_scan(ScanFilter::default()).await?;
-        time::sleep(SCAN_DURATION).await;
-
-        let peripherals = adapter.peripherals().await?;
-        if peripherals.is_empty() {
-            eprintln!("No bluetooth peripherals found, exiting...");
+    let adapter = match adapters.into_iter().next() {
+        Some(adapter) => adapter,
+        None => {
+            eprintln!("No Bluetooth adapters found.");
             return Ok(());
         }
+    };
 
-        let mut progressor_found = false;
-        for peripheral in peripherals.iter() {
-            if let Some(properties) = peripheral.properties().await? {
-                if let Some(name) = properties.local_name {
-                    if name.contains(progressor::NAME) {
-                        if let Err(err) = progressor::connect(peripheral).await {
-                            eprintln!("Error connecting to {}: {}.", name, err);
-                        } else {
-                            println!(
-                                "Connection established. Press Ctrl+C to disconnect and exit."
-                            );
+    println!("Using {}.", adapter.adapter_info().await?);
 
-                            tokio::signal::ctrl_c().await?;
-
-                            peripheral.disconnect().await?;
-                            println!("\nDisconnected, exiting...");
-                        }
-                        progressor_found = true;
-                        break;
-                    }
-                }
-            }
+    if let Some(progressor_peripheral) = progressor::find(&adapter).await? {
+        if let Err(err) = progressor::connect(&progressor_peripheral).await {
+            eprintln!("Error establishing connection: {}.", err);
+        } else {
+            println!("Connection established. Press Ctrl+C to disconnect and exit.");
+            tokio::signal::ctrl_c().await?;
+            progressor_peripheral.disconnect().await?;
+            println!("\nDisconnected.");
         }
-
-        if !progressor_found {
-            eprintln!("Progressor not found, exiting...");
-        }
+    } else {
+        eprintln!("Tindeq Progressor not found.");
     }
+
     Ok(())
 }
--- src/progressor.rs
+++ src/progressor.rs
@@ -1,16 +1,53 @@
-use btleplug::api::Peripheral as _;
-use btleplug::platform::Peripheral;
+use btleplug::api::{Central, Peripheral as _, ScanFilter};
+use btleplug::platform::{Adapter, Peripheral};
 use std::error::Error;
+use std::time::Duration;
+use tokio::time;
 
-pub const NAME: &str = "Progressor";
+const SCAN_DURATION: Duration = Duration::from_secs(10);
+const NAME: &str = "Progressor";
 // pub const SERVICE_UUID: &str = "7e4e1701-1ea6-40c9-9dcc-13d34ffead57";
 // pub const DATA_UUID: &str = "7e4e1702-1ea6-40c9-9dcc-13d34ffead57";
 // pub const CONTROL_POINT_UUID: &str = "7e4e1703-1ea6-40c9-9dcc-13d34ffead57";
 
 /**
+ * Scans for a Tindeq Progressor device using the provided Bluetooth adapter.
+ * # Arguments
+ * * `adapter` - A reference to the Bluetooth adapter to use for scanning.
+ * # Returns
+ * * * `Ok(Some(Peripheral))` - If a Tindeq Progressor device is found.
+ * * `Ok(None)` - If no Tindeq Progressor device is found.
+ * * `Err` - If an error occurs during scanning.
+ */
+pub async fn find(adapter: &Adapter) -> Result<Option<Peripheral>, Box<dyn Error>> {
+    println!(
+        "Scanning for Tindeq Progressor for {} seconds.",
+        SCAN_DURATION.as_secs()
+    );
+    adapter.start_scan(ScanFilter::default()).await?;
+    time::sleep(SCAN_DURATION).await;
+
+    for peripheral in adapter.peripherals().await? {
+        if let Some(properties) = peripheral.properties().await? {
+            if let Some(name) = properties.local_name {
+                if name.contains(NAME) {
+                    println!("Found Tindeq Progressor: {}", name);
+                    return Ok(Some(peripheral));
+                }
+            }
+        }
+    }
+
+    Ok(None)
+}
+
+/**
  * Handles connection and interaction with a Tindeq Progressor device.
  * # Arguments
  * * `progressor` - A reference to the Peripheral representing the Tindeq Progressor device.
+ * # Returns
+ * * * `Ok(())` - If the connection and interaction are successful.
+ * * `Err` - If an error occurs during connection or interaction.
  */
 pub async fn connect(progressor: &Peripheral) -> Result<(), Box<dyn Error>> {
     let properties = progressor
@@ -26,23 +63,24 @@ pub async fn connect(progressor: &Peripheral) -> Result<(), Box<dyn Error>> {
     progressor.connect().await?;
 
     let is_connected = progressor.is_connected().await?;
-    if is_connected {
-        // println!("Discovering services for {}", &name);
-        // peripheral.discover_services().await?;
-
-        // for service in peripheral.services() {
-        //     println!(
-        //         "  Service UUID: {}, Primary: {}",
-        //         service.uuid, service.primary
-        //     );
-        //     for characteristic in service.characteristics {
-        //         println!("    Characteristic: {:?}", characteristic);
-        //     }
-        // }
-
-        // TODO: Do something useful with the Progressor device here
-    } else {
+    if !is_connected {
         return Err("Failed to connect.".into());
     }
+
+    // println!("Discovering services for {}", &name);
+    // peripheral.discover_services().await?;
+
+    // for service in peripheral.services() {
+    //     println!(
+    //         "  Service UUID: {}, Primary: {}",
+    //         service.uuid, service.primary
+    //     );
+    //     for characteristic in service.characteristics {
+    //         println!("    Characteristic: {:?}", characteristic);
+    //     }
+    // }
+
+    // TODO: Do something useful with the Progressor device here
+
     Ok(())
 }
